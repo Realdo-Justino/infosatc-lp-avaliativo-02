35a6b7b8c1b0afcc494baa9d9fb7fb3492439cf1
Realdo-Justino <realdojj@gmail.com>
2020-09-05 11:22:37 -0300

    feat: exercicio-05

---
+++ exercicio-05.py
@@ -0,0 +1,5 @@
+listaPecas=["Sei","nada","de","pc","gaming","o","meu","é","do","inicio","dos","anos","2000","gabinete cooler master","placa de vídeo GeForce RTX 2080Ti"]
+listaPecas.pop(13)
+listaPecas.pop(13)
+
+print(listaPecas)
