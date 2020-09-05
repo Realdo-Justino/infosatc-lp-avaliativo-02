ec3abdcc9b0426d146428126cc3dbfa242946459
Realdo-Justino <realdojj@gmail.com>
2020-09-05 11:17:51 -0300

    feat: exercicio-03

---
+++ exercicio-03.py
@@ -0,0 +1,6 @@
+valor=(float(input("Insira um valor a buscar na lista ")))
+lista=[1,3,5,7,9]
+if valor in lista:
+    print("O valor ",valor," foi enconttrado")
+else:
+    print("O valor ",valor," não foi encontrado")
