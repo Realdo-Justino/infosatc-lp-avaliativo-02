88cca835a2b92af6372112a4da4eea17d919c356
Realdo-Justino <realdojj@gmail.com>
2020-09-05 11:19:04 -0300

    exercicio-04

---
+++ exercicio-04.py
@@ -0,0 +1,7 @@
+listaFilmes=["Kill Bill","The Thing"]
+listaJogos=["Dudes with attitude","Dr Jekyll and Mr Hyde"]
+listaLivros=["O Alienista","David Copperfield"]
+listaEsportes=["Esgrima","Judo"]
+listaTotal=listaEsportes+listaFilmes+listaJogos+listaLivros
+
+print("Um item da tabela de livros é: ",listaLivros[1])
